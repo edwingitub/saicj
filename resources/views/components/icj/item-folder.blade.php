d7b4002fb46dfe34b355cd0ad0d5a1e013ce970d
Edwin Monterroza <edwincphll@gmail.com>
2023-03-24 22:49:38 -0600

    ok

--- resources/views/components/icj/item-folder.blade.php
+++ resources/views/components/icj/item-folder.blade.php
@@ -1,10 +1,10 @@
 <div class="lg:w-1/4 w-full ">
-    <div class="border-gray-200       border-gray-300 shadow-lg hover:scale-105 hover:shadow-2xl border-b-2  border-orange-400  ">
+    <div class=" shadow-lg hover:scale-105 hover:shadow-2xl border-b-2  border-orange-400  ">
 
         <div class="h-5 w-32 bg-orange-300 rounded-tr-lg"></div>
 
         <!-- tema -->
-        <div class="bg-orange-300 font-bold  text-orange-900 p-2   bg-orange-300 border-b-2  border-orange-400">
+        <div class=" font-bold  text-orange-900 p-2   bg-orange-300 border-b-2  border-orange-400">
           {{$header}}
         </div>
          <!-- detalle -->
