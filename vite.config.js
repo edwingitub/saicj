f86cec489e8579b9746f0abd8164f0dce8bd54be
Edwin Monterroza <edwincphll@gmail.com>
2023-05-04 17:26:23 -0600

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-
+    server: {
+        host:'192.168.0.100',  // Add this to force IPv4 only
+    },
     plugins: [
         laravel({
             input: [
